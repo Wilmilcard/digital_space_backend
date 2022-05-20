954068633e577a03f4bf8af98b924442fdae21ba
Wilmilcard <leonjuandavid@hotmail.com>
2022-05-20 08:07:27 -0500

    ajustes

--- DigitalSpaceBN/Program.cs
+++ DigitalSpaceBN/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace DigitalSpaceBN
@@ -14,7 +16,9 @@ namespace DigitalSpaceBN
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
